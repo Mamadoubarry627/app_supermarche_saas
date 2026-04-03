baae8a18fbafccbabc400db2551b21785ebee365
Mamadou Barry <mb817026@gmail.com>
2026-04-03 15:02:41 +0000

    correction templates

--- market/settings.py
+++ market/settings.py
@@ -228,9 +228,9 @@ LOGIN_REDIRECT_URL = '/'
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": "dlygyralg",
-    "API_KEY": "846575384269455",
-    "API_SECRET": "r9XrD5MlSbbQsmNoNylXMIDBhms",
+    "CLOUD_NAME": os.getenv("CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
 }
 
 #MEDIA_URL = '/media/'
