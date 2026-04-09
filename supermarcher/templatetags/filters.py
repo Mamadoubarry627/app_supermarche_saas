b6c6b37a286c34da301bbd2cea995331465cf416
Mamadou Barry <mb817026@gmail.com>
2026-04-09 18:03:09 +0000

    correction templates

--- supermarcher/templatetags/filters.py
+++ supermarcher/templatetags/filters.py
@@ -5,3 +5,26 @@ register = template.Library()
 @register.filter
 def underscore_to_space(value):
     return value.replace("_", " ")
+
+from django import template
+
+register = template.Library()
+
+
+def is_numeric_field(field):
+    numeric_fields = [
+        "prix",
+        "prix_unitaire",
+        "prix_achat",
+        "prix_vente",
+        "total",
+        "montant",
+        "montant_total",
+        "quantite",
+        "quantite_stock",
+        "total_ventes",
+        "total_ca",
+        "marge",
+        "benefice"
+    ]
+    return field in numeric_fields
